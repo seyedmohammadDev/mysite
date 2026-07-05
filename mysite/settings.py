be1440baac5004504756ada5a600a3e278f48048
seyedMohammad_0rg <seyedmohammad.dev@gmail.com>
2026-07-05 11:10:03 +0330

    general static
    adding general static to the project

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,3 +116,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics'
+]
+
+
