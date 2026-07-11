4d33f77ba3f287f1af24067b01582fc5682ed9e3
seyedMohammad_0rg <seyedmohammad.dev@gmail.com>
2026-07-11 11:47:49 +0330

    customizing admin-panel
    customizing admin-panel
    use class for showing date_hierarchy

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+admin.site.register(Post,PostAdmin)
